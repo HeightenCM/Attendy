30807591f6eb6752d65aba1e8381f3320cfdd4ab
Ali Hassan <hassanali22@stud.ase.ro>
2025-01-15 17:22:05 +0200

    sync

--- setup_dev_env_files/run_me_if_windows.bat
+++ setup_dev_env_files/run_me_if_windows.bat
@@ -5,5 +5,5 @@ npm i
 npm i --save-dev nodemon
 echo Installation complete!
 echo Installating frontend dependencies...
-echo Use 'npm install', 'npm i -D react-router-dom', npm install jwt-decode  in the frontend folder
+echo Use 'npm install', 'npm i -D react-router-dom', 'npm install jwt-decode'  in the frontend folder
 pause
