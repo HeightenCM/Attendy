90915f022826535d23c632b77443cbad4b06fd28
HeightenCM <hasconstantin@proton.me>
2025-01-21 00:24:08 +0200

    attendee is displayed with email and join time, removed xlsx

--- frontend/src/components/AttendeeList.jsx
+++ frontend/src/components/AttendeeList.jsx
@@ -6,57 +6,57 @@ import xlsx from "json-as-xlsx";
 const AttendeeList = (attendees) => {
   attendees = attendees.attendees;
 
-  function convertToXlsx(attendees) {
-    const dataForXlsx = [
-      {
-        sheet: "Attendees",
-        columns: [
-          {
-            label: "Email",
-            value: "email",
-          },
-          { label: "Name", value: "name" },
-        ],
-        content: attendees,
-      },
-    ];
+  // function convertToXlsx(attendees) {
+  //   const dataForXlsx = [
+  //     {
+  //       sheet: "Attendees",
+  //       columns: [
+  //         {
+  //           label: "Email",
+  //           value: "email",
+  //         },
+  //         { label: "Name", value: "name" },
+  //       ],
+  //       content: attendees,
+  //     },
+  //   ];
 
-    // [
-    //   {
-    //     sheet: "Adults",
-    //     columns: [
-    //       { label: "User", value: "user" }, // Top level data
-    //       { label: "Age", value: (row) => row.age + " years" }, // Custom format
-    //       { label: "Phone", value: (row) => (row.more ? row.more.phone || "" : "") }, // Run functions
-    //     ],
-    //     content: [
-    //       { user: "Andrea", age: 20, more: { phone: "11111111" } },
-    //       { user: "Luis", age: 21, more: { phone: "12345678" } },
-    //     ],
-    //   },
-    //   {
-    //     sheet: "Children",
-    //     columns: [
-    //       { label: "User", value: "user" }, // Top level data
-    //       { label: "Age", value: "age", format: '# "years"' }, // Column format
-    //       { label: "Phone", value: "more.phone", format: "(###) ###-####" }, // Deep props and column format
-    //     ],
-    //     content: [
-    //       { user: "Manuel", age: 16, more: { phone: 9999999900 } },
-    //       { user: "Ana", age: 17, more: { phone: 8765432135 } },
-    //     ],
-    //   },
-    // ]
+  //   // [
+  //   //   {
+  //   //     sheet: "Adults",
+  //   //     columns: [
+  //   //       { label: "User", value: "user" }, // Top level data
+  //   //       { label: "Age", value: (row) => row.age + " years" }, // Custom format
+  //   //       { label: "Phone", value: (row) => (row.more ? row.more.phone || "" : "") }, // Run functions
+  //   //     ],
+  //   //     content: [
+  //   //       { user: "Andrea", age: 20, more: { phone: "11111111" } },
+  //   //       { user: "Luis", age: 21, more: { phone: "12345678" } },
+  //   //     ],
+  //   //   },
+  //   //   {
+  //   //     sheet: "Children",
+  //   //     columns: [
+  //   //       { label: "User", value: "user" }, // Top level data
+  //   //       { label: "Age", value: "age", format: '# "years"' }, // Column format
+  //   //       { label: "Phone", value: "more.phone", format: "(###) ###-####" }, // Deep props and column format
+  //   //     ],
+  //   //     content: [
+  //   //       { user: "Manuel", age: 16, more: { phone: 9999999900 } },
+  //   //       { user: "Ana", age: 17, more: { phone: 8765432135 } },
+  //   //     ],
+  //   //   },
+  //   // ]
 
-    let settings = {
-      fileName: "Event",
-      extraLength: 3,
-      writeMode: "writeFile",
-      writeOptions: {},
-      RTL: false,
-    };
-    xlsx(dataForXlsx, settings);
-  }
+  //   let settings = {
+  //     fileName: "Event",
+  //     extraLength: 3,
+  //     writeMode: "writeFile",
+  //     writeOptions: {},
+  //     RTL: false,
+  //   };
+  //   xlsx(dataForXlsx, settings);
+  // }
 
   return (
     <>
@@ -78,13 +78,13 @@ const AttendeeList = (attendees) => {
                   .CSV
                 </button>
               </CSVLink>
-              <button
+              {/* <button
                 onClick={convertToXlsx(attendees)}
                 type="button"
                 className="btn btn-success ms-3"
               >
                 .XLSX
-              </button>
+              </button> */}
               {/* <button
                 type="button"
                 className="btn btn-primary ml-3"
@@ -101,7 +101,7 @@ const AttendeeList = (attendees) => {
                 {attendees &&
                   attendees.map((attendee) => (
                     <li className="list-group-item" key={attendee.email}>
-                      {attendee.name}
+                      {attendee.name} - {attendee.email} - {attendee.time}
                     </li>
                   ))}
               </ul>
